eca04f07d200d397360164c808f33399056cc426
Sergen Bal <sergenbal.sb@gmail.com>
2017-03-03 12:30:23 +0200

    Create pow2.cpp

---
+++ pow2.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char *argv[])
+{
+    
+    int i,sonuc=1;
+    cout<<"taban sayisini giriniz:";
+    int a;
+    cin>>a;
+    cout<<"üst sayiyi giriniz:";
+    int b;
+    cin>> b;
+    
+    for(i=0;i<b;i++){
+        sonuc=sonuc*a;                 
+    }
+    cout<< sonuc <<endl;
+    
+   
+   
+  
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
+ 
